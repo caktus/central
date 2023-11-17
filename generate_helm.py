d1c39ec4b1824b3457206b539a39074c679492b2
Tobias McNulty <tobias@caktusgroup.com>
2023-11-17 16:15:29 -0500

    remove underscores

--- generate_helm.py
+++ generate_helm.py
@@ -22,6 +22,17 @@ def main():
     for svc in ghcr_services:
         yml["services"][svc].pop("build")
         yml["services"][svc]["image"] = f"ghcr.io/caktus/central-{svc}"
+    rename_services = {
+        "enketo_redis_main": "enketo-redis-main",
+        "enketo_redis_cache": "enketo-redis-cache",
+    }
+    for old, new in rename_services.items():
+        yml["services"][new] = yml["services"].pop(old)
+    for svc in yml["services"].values():
+        for old, new in rename_services.items():
+            if "depends_on" in svc and old in svc["depends_on"]:
+                svc["depends_on"].remove(old)
+                svc["depends_on"].append(new)
     print(oyaml.dump(yml))
 
 
